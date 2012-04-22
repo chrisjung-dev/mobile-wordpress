6f889c8ba9cc040e6214d801d955d13cd81252db
Chris Jung <campino2k@gmail.com>
2012-04-22 16:51:06 +0200

    Prepare Categories
    - added category filter page
    - made category button functional
    - TODO: List categories and link

---
+++ categories.php
@@ -0,0 +1,46 @@
+<?php
+if( isset( $_GET[ 'cat' ] ) ) {
+	$category_id = $_GET[ 'cat' ];
+}
+?>
+<?php include( 'inc/header.php' ); ?>
+			
+		<?php
+		if( isset( $category_id ) ) :
+			/*
+				Load categories
+			*/
+			query_posts( 'cat=' . $category_id );
+			if (have_posts()) :
+			?>
+			<ul data-role="listview" data-theme="c">
+			<?php
+				while (have_posts()) : the_post(); ?>
+
+				<li>
+					<a href="./single.php?p=<?php the_ID(); ?>"><?php the_title(); ?></a>
+					<span class="ui-li-count"><?php comments_number( '0', '1', '%' ); ?></span>
+				</li>
+
+			<?php 
+				endwhile; 
+			?>
+			</ul>
+			<?php
+			else:
+			?>
+
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			
+			<?php 
+			endif; 
+			wp_reset_query();
+
+		else :
+		?>
+			List categories
+		<?php
+		endif;
+		?>
+
+<?php include( 'inc/footer.php' ); ?>
--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,7 @@
 				<div data-role="navbar">
 					<ul>
 						<li><a href="#" data-icon="info"><?php echo $lng[ 'btn_info' ];?></a></li>
-						<li><a href="#" data-icon="grid"><?php echo $lng[ 'btn_categories' ];?></a></li>
+						<li><a href="categories.php" data-icon="grid"><?php echo $lng[ 'btn_categories' ];?></a></li>
 						<li><a href="#" data-icon="gear"><?php echo $lng[ 'btn_desktop' ];?></a></li>
 					</ul>
 				</div><!-- /navbar -->
